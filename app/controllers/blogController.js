e8625a34d1f186ef1a8a94824b6d686afb72283e
Abdur-Rouf-Robin <abdurrouf.ruet.cse@gmail.com>
2024-11-07 20:41:09 +0600

    last commit

---
+++ app/controllers/blogController.js
@@ -0,0 +1,15 @@
+exports.createBlog = (req, res) => {
+  res.status(201).send('Blog created successfully');
+};
+
+exports.readBlog = (req, res) => {
+  res.status(200).send('Blog read successfully');
+};
+
+exports.updateBlog = (req, res) => {
+  res.status(200).send('Blog update successfully');
+};
+
+exports.deleteBlog = (req, res) => {
+  res.status(200).send('Blog delete successfully');
+};
